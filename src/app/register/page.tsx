a9747e647ca43f021b30c2e91a7584f0142d9958
mselbekk11 <mselbekk11@gmail.com>
2025-02-19 15:34:03 -0800

    updates

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -19,6 +19,7 @@ import { useToast } from '@/hooks/use-toast';
 import { Button } from '@/components/ui/button';
 import { FlickeringGrid } from '@/components/magicui/flickering-grid';
 import ScrambleText from '@/components/scramble-text';
+import Spinner from '@/components/spinner';
 
 export default function Register() {
   // Hooks for wallet connection/disconnection
@@ -200,26 +201,7 @@ export default function Register() {
                   {isRegistering ? (
                     <>
                       <span className='mr-2'>Registering</span>
-                      <svg
-                        className='animate-spin h-5 w-5'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                      >
-                        <circle
-                          className='opacity-25'
-                          cx='12'
-                          cy='12'
-                          r='10'
-                          stroke='currentColor'
-                          strokeWidth='4'
-                        />
-                        <path
-                          className='opacity-75'
-                          fill='currentColor'
-                          d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                        />
-                      </svg>
+                      <Spinner />
                     </>
                   ) : (
                     'Register Wallet'
