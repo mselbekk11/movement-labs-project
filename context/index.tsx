ec61234bdd7c62eb39d672cbe97547e4781bc221
mselbekk11 <mselbekk11@gmail.com>
2025-02-18 18:47:53 -0800

    updates

--- context/index.tsx
+++ context/index.tsx
@@ -32,10 +32,18 @@ const modal = createAppKit({
   networks: [mainnet],
   defaultNetwork: mainnet,
   metadata: metadata,
+  enableWalletConnect: false,
   features: {
-    analytics: true, // Optional - defaults to your Cloud configuration
+    analytics: true,
+    email: false,
+    socials: false,
   },
   themeMode: 'dark',
+  allWallets: 'HIDE',
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+    '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709', // OKX
+  ],
 });
 
 function ContextProvider({
